c5506742ab31f9ed97c909ec3e55c04609800686
Elgun <elgun1999@gmail.com>
2020-08-07 21:11:00 +0400

    add tests & refactor index.ts

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,50 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 400 on supplied email does not exists', async () => {
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('fails when an incorrect pass is supplied', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      emai: 'test@test.com',
+      password: 'ewfwef',
+    })
+    .expect(400);
+});
+
+it('responds with a cookie with valid credentials', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(200);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
